59682979de3ef2368fcaf4379798affb694cf941
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-05-01 12:53:22 +0530

    added route protection using clerkMiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,16 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware()
-
-const isProtectedRoute = createRouteMatcher([
-  "/",
-  "/test(.*)"
+const isProtectedRoutes = createRouteMatcher([
+  '/',
+  '/test',
 ])
 
+// This Middleware does not protect any routes by default.
+// See https://clerk.com/docs/references/nextjs/clerk-middleware for more information about configuring your Middleware
+export default clerkMiddleware((auth, req)=>{
+  if(isProtectedRoutes(req)) auth().protect();
+});
+
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
-  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
