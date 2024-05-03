aa654834a26fd063cd0b36d1a861fca03559dc42
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-05-03 22:22:32 +0530

    course creation form

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image"
+import Image from "next/image";
 
 export const Logo = () => {
-    return(
-        <Image 
-            height={30}
-            width={30}
-            alt="logo"
-            src="/logo.svg"
-        />
-    )
-}
+	return (
+		<div className="flex items-center justify-start gap-3 ml-1">
+			<Image height={30} width={30} alt="logo" src="/logo.svg" />
+			<span className="text-xl font-bold text-[#0369a1]">Coursify</span>
+		</div>
+	);
+};
