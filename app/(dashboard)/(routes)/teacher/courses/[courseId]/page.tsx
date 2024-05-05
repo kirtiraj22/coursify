193bde3632e76f075bf690cc1e095f0ddb1699e4
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-05-05 12:06:19 +0530

    added customized icon badge

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -48,6 +48,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 			<div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
 				<div>
 					<div className="flex items-center gap-x-2">
+                        <IconBadge icon={LayoutDashboard}/>
 						<h2 className="text-xl">Customize your course</h2>
 					</div>
 				</div>
