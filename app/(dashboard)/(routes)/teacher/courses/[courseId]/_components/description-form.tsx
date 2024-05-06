150a690a46c2f2f041f2e9c8c6604202417d1efb
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-05-06 12:47:37 +0530

    added course description field

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+interface DescriptionFormProps {
+	initialData: {
+		description: string;
+	};
+	courseId: string;
+}
+
+const formSchema = z.object({
+	description: z.string().min(1, {
+		message: "Description is required",
+	}),
+});
+
+export const DescriptionForm = ({
+	initialData,
+	courseId,
+}: DescriptionFormProps) => {
+	const [isEditing, setIsEditing] = useState(false);
+
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: initialData,
+	});
+
+	const { isSubmitting, isValid } = form.formState;
+
+	const toggleEdit = () => setIsEditing((current) => !current);
+
+	const onSubmit = (values: z.infer<typeof formSchema>) => {
+		console.log(values);
+	};
+
+	return (
+		<div className="mt-6 border bg-slate-100 rounded-md p-4">
+			<div className="font-medium flex items-center justify-between">
+				Course description
+				<Button onClick={toggleEdit} variant="ghost">
+					{isEditing ? (
+						<>Cancel </>
+					) : (
+						<>
+							<Pencil className="h-4 w-4 mr-2" />
+							Edit Description
+						</>
+					)}
+				</Button>
+			</div>
+			{!isEditing && (
+				<p
+					className={cn(
+						"text-sm mt-2",
+						!initialData.description && "text-slate-500 italic"
+					)}
+				>
+					{initialData?.description || "No description"}
+				</p>
+			)}
+			{isEditing && (
+				<Form {...form}>
+					<form
+						onSubmit={form.handleSubmit(onSubmit)}
+						className="space-y-4 mt-4"
+					>
+						<FormField
+							control={form.control}
+							name="description"
+							render={({ field }) => (
+								<FormItem>
+									<FormControl>
+										<Textarea
+                                            disabled={isSubmitting}
+                                            placeholder="This course is about..."
+                                            {...field}
+										/>
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+						<div className="flex items-center gap-x-2">
+							<Button
+								disabled={!isValid || isSubmitting}
+								type="submit"
+							>
+								Save
+							</Button>
+						</div>
+					</form>
+				</Form>
+			)}
+		</div>
+	);
+};
