6d30490352d78674c21e16626475a7f432985a9c
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-05-17 23:50:17 +0530

    POST && DELETE endpoint for course attachments

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -4,8 +4,7 @@ import axios from "axios";
 import { useState } from "react";
 import * as z from "zod";
 import toast from "react-hot-toast";
-import Image from "next/image";
-import { ImageIcon, Pencil, PlusCircle } from "lucide-react";
+import { File, Loader2, PlusCircle, X } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { Attachment, Course } from "@prisma/client";
 
@@ -26,21 +25,35 @@ export const AttachmentForm = ({
 	courseId,
 }: AttachmentFormProps) => {
 	const [isEditing, setIsEditing] = useState(false);
+	const [deletingId, setDeletingId] = useState<string | null>(null);
 
 	const router = useRouter();
 	const toggleEdit = () => setIsEditing((current) => !current);
 
 	const onSubmit = async (values: z.infer<typeof formSchema>) => {
 		try {
-			await axios.patch(`/api/courses/${courseId}`, values);
-			toast.success("Description updated Successfully");
+			await axios.post(`/api/courses/${courseId}/attachments`, values);
+			toast.success("Attachment added Successfully");
 			toggleEdit();
 			router.refresh();
 		} catch (error) {
-			console.log("[DESCRIPTION FORM]", error);
+			console.log("[ATTACHMENT FORM]", error);
 			toast.error("Something went wrong!");
 		}
 	};
+
+	const onDelete = async (id: string) => {
+		try{
+			setDeletingId(id);
+			await axios.delete(`/api/courses/${courseId}/attachments/${id}`)
+			toast.success("Attachment deleted!")
+			router.refresh();
+		}catch{
+			toast.error("Something went wrong!")
+		}finally{
+			setDeletingId(null)
+		}
+	}
 
 	return (
 		<div className="mt-6 border bg-slate-100 rounded-md p-4">
@@ -63,6 +76,31 @@ export const AttachmentForm = ({
 						<p className="text-sm mt-2 text-slate-500 italic">
 							No attachments yet
 						</p>
+					)}
+					{initialData.attachments.length > 0 && (
+						<div className="space-y-2">
+							{initialData.attachments.map((attachment) => (
+								<div
+									key={attachment.id}
+									className="flex items-center p-3 w-full bg-sky-100 border-sky-200 border text-sky-700 rounded-md"
+								>
+									<File className="h-4 w-4 mr-2 flex-shrink-0" />
+									<p className="text-xs line-clamp-1">
+										{attachment.name}
+									</p>
+									{deletingId === attachment.id && (
+										<div>
+											<Loader2 className="h-4 w-4 animate-spin" />
+										</div>
+									)}
+									{deletingId !== attachment.id && (
+										<button onClick={()=> onDelete(attachment.id)} className="ml-auto hover:opacity-75 transition">
+											<X className="h-4 w-4" />
+										</button>
+									)}
+								</div>
+							))}
+						</div>
 					)}
 				</>
 			)}
