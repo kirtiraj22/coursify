94338f4d45a3064bdfd0477e0ecd9120ae841957
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-05-17 22:57:22 +0530

    added course attachment component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -2,13 +2,19 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
-import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
+import {
+	CircleDollarSign,
+	File,
+	LayoutDashboard,
+	ListChecks,
+} from "lucide-react";
 import { IconBadge } from "@/components/icon-badge";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
+import { AttachmentForm } from "./_components/attachment-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 	const { userId } = auth();
@@ -21,6 +27,13 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 		where: {
 			id: params.courseId,
 		},
+		include: {
+			attachments: {
+				orderBy: {
+					createdAt: "desc"
+				}
+			}
+		}
 	});
 
 	const categories = await db.category.findMany({
@@ -93,6 +106,13 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 							<h2 className="text-xl">Sell your course</h2>
 						</div>
 						<PriceForm initialData={course} courseId={course.id} />
+					</div>
+					<div>
+						<div className="flex items-center gap-x-2">
+							<IconBadge icon={File} />
+							<h2 className="text-xl">Resources & Attachments</h2>
+						</div>
+						<AttachmentForm initialData={course} courseId={course.id} />
 					</div>
 				</div>
 			</div>
