d7ca36e9bc4c59c4b64c676218e942af8869f028
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-05-23 14:24:41 +0530

    chapter accessibility form(isFree or not)

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
-import { ArrowLeft, LayoutDashboard } from "lucide-react";
+import { ArrowLeft, Eye, LayoutDashboard, Video } from "lucide-react";
 import { IconBadge } from "@/components/icon-badge";
 import { ChapterTitleForm } from "./_components/chapter-title-form";
 import { ChapterDescriptionForm } from "./_components/chapter-description-form";
+import { ChapterAccessForm } from "./_components/chapter-access-form";
 
 const ChapterIdPage = async ({
 	params,
@@ -58,40 +59,49 @@ const ChapterIdPage = async ({
 						<ArrowLeft className="h-4 w-4 mr-2" />
 						Back to course setup
 					</Link>
-                    <div className="flex items-center justify-between w-full">
-                        <div className="flex flex-col gap-y-2">
-                            <h1 className="text-2xl font-medium">
-                                Chapter Creation
-                            </h1>
-                            <span className="text-sm text-slate-700">
-                                Complete all fields {completionText}
-                            </span>
-                        </div>
-                    </div>
+					<div className="flex items-center justify-between w-full">
+						<div className="flex flex-col gap-y-2">
+							<h1 className="text-2xl font-medium">
+								Chapter Creation
+							</h1>
+							<span className="text-sm text-slate-700">
+								Complete all fields {completionText}
+							</span>
+						</div>
+					</div>
 				</div>
 			</div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-                <div className="space-y-4">
-                    <div>
-                        <div className="flex items-center gap-x-2">
-                            <IconBadge icon={LayoutDashboard}/>
-                            <h2 className="text-xl"> 
-                                Customize your chapter
-                            </h2>
-                        </div>
-                        <ChapterTitleForm 
-                            initialData={chapter}
-                            courseId={params.courseId}
-                            chapterId={params.chapterId}
-                        />
-						<ChapterDescriptionForm 
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+				<div className="space-y-4">
+					<div>
+						<div className="flex items-center gap-x-2">
+							<IconBadge icon={LayoutDashboard} />
+							<h2 className="text-xl">Customize your chapter</h2>
+						</div>
+						<ChapterTitleForm
 							initialData={chapter}
 							courseId={params.courseId}
 							chapterId={params.chapterId}
 						/>
-                    </div>
-                </div>
-            </div>
+						<ChapterDescriptionForm
+							initialData={chapter}
+							courseId={params.courseId}
+							chapterId={params.chapterId}
+						/>
+					</div>
+					<div>
+						<div className="flex items-center gap-x-2">
+							<IconBadge icon={Eye} />
+							<h2 className="text-xl">Access Settings</h2>
+						</div>
+						<ChapterAccessForm 
+							initialData={chapter}
+							courseId={params.courseId}
+							chapterId={params.chapterId}
+						/>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
