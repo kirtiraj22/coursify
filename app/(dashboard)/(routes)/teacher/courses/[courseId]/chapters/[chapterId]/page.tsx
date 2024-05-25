d2c5125b2ec5c8a571d970b596531830fabb3717
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-05-25 23:28:09 +0530

    initial chapter actions

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -10,6 +10,7 @@ import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 import { ChapterAccessForm } from "./_components/chapter-access-form";
 import { ChapterVideoForm } from "./_components/chapter-video-form";
 import { Banner } from "@/components/banner";
+import { ChapterActions } from "./_components/chapter-actions";
 
 const ChapterIdPage = async ({
 	params,
@@ -50,6 +51,8 @@ const ChapterIdPage = async ({
 
 	const completionText = `(${completedFields}/${totalFields})`;
 
+	const isComplete = requiredFields.every(Boolean)
+
 	return (
 		<>
 		{!chapter.isPublished && (
@@ -77,6 +80,12 @@ const ChapterIdPage = async ({
 									Complete all fields {completionText}
 								</span>
 							</div>
+							<ChapterActions 
+								disabled={!isComplete}
+								courseId={params.courseId}
+								chapterId={params.chapterId}
+								isPublished={chapter.isPublished}
+							/>
 						</div>
 					</div>
 				</div>
