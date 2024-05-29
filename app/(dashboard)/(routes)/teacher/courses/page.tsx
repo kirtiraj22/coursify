8fc6b13ed1cfa4aa8442d56fa5ec7a02ce186d39
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-05-29 11:49:16 +0530

    added data table(shadcn) for user's courses

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,38 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 
-const CoursesPage = () => {
+import { Button } from "@/components/ui/button";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { db } from "@/lib/db";
+
+const CoursesPage = async () => {
+	const { userId } = auth();
+
+	if(!userId){
+		return redirect("/")
+	}
+
+	const courses = await db.course.findMany({
+		where: {
+			userId: userId,
+		},
+		orderBy: {
+			createdAt: "desc"
+		}
+	})
+
 	return (
 		<div className="p-6">
-			<Link href="/teacher/create">
+			{/* <Link href="/teacher/create">
 				<Button>New Course</Button>
-			</Link>
+			</Link> */}
+
+			<DataTable 
+				columns={columns}
+				data={courses}
+			/>
 		</div>
 	);
 };
